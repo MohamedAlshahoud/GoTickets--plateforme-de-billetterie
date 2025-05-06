953e673a9d8146bd8f14dfc871eb04bd3fe40c53
Mohamed Alshahoud <ay1665ay@gmail.com>
2025-05-06 19:53:32 +0200

    Ajout du champ is_admin et restriction de l'accès au panneau Filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    public function canAccessPanel(\Filament\Panel $panel): bool
+    {
+        return $this->is_admin;
+    }
 }
